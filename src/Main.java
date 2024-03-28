a77968c553bb4dd5073c0689799bbf4149b42a27
Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>
2024-03-28 02:57:00 -0300

    100% correto e confirmado, 0 erros tratados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 import Criptografia.AES;
 import Criptografia.Chaves;
 import Criptografia.Vernam;
-import Gateway.GatewayInterface;
+import Banco.BancoInterface;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -10,14 +9,18 @@ import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 public class Main {
-    static GatewayInterface stub;
-    static String ipCliente = "5557";
+    static BancoInterface stub;
+    static String ipCliente = "5552";
+    static String nome = null;
+    static String cpf = null;
+
+    static boolean autenticado = false;
 
     public static void main(String[] args) throws Exception {
 
         try {
             Registry registro = LocateRegistry.getRegistry("localhost", 6002);
-            stub = (GatewayInterface) registro.lookup("Gateway");
+            stub = (BancoInterface) registro.lookup("Banco");
 
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
@@ -33,28 +36,175 @@ public class Main {
 
 
         stub.receberChave(ipCliente, chave);
+
+
         while (!parar) {
-            System.out.println("Bem vindo, você está conectado a porta: " + ipCliente + ".");
-            System.out.println("Escolha uma opção: ");
+            String cpfCriptografado;
+
+            if (autenticado) {
+                System.out.println("Bem vindo, "+ nome + ".");
+                System.out.println("Escolha uma opção: ");
+                System.out.println("1 - Ver Saldo");
+                System.out.println("2 - Sacar");
+                System.out.println("3 - Depositar");
+                System.out.println("4 - Transferir");
+                System.out.println("5 - Investir");
+                System.out.println("6 - Sair");
+
+                int opcao = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcao) {
+
+                    case 1:
+                        cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                        cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+
+                        try {
+                            String saldoDescriptografado = stub.getSaldo(ipCliente, cpfCriptografado);
+                            saldoDescriptografado = AES.decifrar(saldoDescriptografado, chave.CHAVE_AES);
+                            saldoDescriptografado = Vernam.decifrar(saldoDescriptografado, chave.CHAVE_VERNAM);
+
+                            System.out.println("Seu saldo é de " + saldoDescriptografado + " dinheiros.");
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                    case 2:
+                    System.out.println("Digite o valor: ");
+                    String valor = scanner.nextLine();
+
+                    cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                    cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+                    String valorCriptografado = Vernam.cifrar(valor, chave.CHAVE_VERNAM);
+                    valorCriptografado = AES.cifrar(valorCriptografado, chave.CHAVE_AES);
+
+                    try {
+                        String saque = stub.sacar(ipCliente, cpfCriptografado, valorCriptografado);
+                        saque = AES.decifrar(saque, chave.CHAVE_AES);
+                        saque = Vernam.decifrar(saque, chave.CHAVE_VERNAM);
+                        saque = saque.split("-")[4];
+                        System.out.println("Você sacou " + valor + " dinheiros, seu novo saldo é de " + saque);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Digite o valor: ");
+                    valor = scanner.nextLine();
+
+                    cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                    cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+                     valorCriptografado = Vernam.cifrar(valor, chave.CHAVE_VERNAM);
+                    valorCriptografado = AES.cifrar(valorCriptografado, chave.CHAVE_AES);
+
+                    try {
+                       String depositar = stub.depositar(ipCliente, cpfCriptografado, valorCriptografado);
+                       depositar = AES.decifrar(depositar, chave.CHAVE_AES);
+                       depositar = Vernam.decifrar(depositar, chave.CHAVE_VERNAM);
+                       depositar = depositar.split("-")[4];
+
+
+                       System.out.println("Você depositou " + valor + "dinheiros, seu novo saldo é de: " + depositar);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                    case 4:
+                    String contaOrigem = cpf;
+                    System.out.println("Digite a conta de destino: ");
+                    String contaDestino = scanner.nextLine();
+                    System.out.println("Digite o valor: ");
+                    valor = scanner.nextLine();
+
+                    String contaOrigemCriptografada = Vernam.cifrar(contaOrigem, chave.CHAVE_VERNAM);
+                    contaOrigemCriptografada = AES.cifrar(contaOrigemCriptografada, chave.CHAVE_AES);
+                    String contaDestinoCriptografada = Vernam.cifrar(contaDestino, chave.CHAVE_VERNAM);
+                    contaDestinoCriptografada = AES.cifrar(contaDestinoCriptografada, chave.CHAVE_AES);
+                    valorCriptografado = Vernam.cifrar(valor, chave.CHAVE_VERNAM);
+                    valorCriptografado = AES.cifrar(valorCriptografado, chave.CHAVE_AES);
+
+                    try {
+                        String transferiu = stub.transferir(ipCliente, contaOrigemCriptografada, contaDestinoCriptografada, valorCriptografado);
+                        transferiu = AES.decifrar(transferiu, chave.CHAVE_AES);
+                        transferiu = Vernam.decifrar(transferiu, chave.CHAVE_VERNAM);
+                        System.out.println("Você transferiu " + valor + " dinheiros para a conta de " + transferiu.split("-")[1] + " seu novo saldo é de: " + transferiu.split("-")[0] + ".");
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+
+                    case 5:
+                        System.out.println("Escolha uma opção de investimento: ");
+                        System.out.println("1 - Poupança");
+                        System.out.println("2 - Renda fixa");
+                        System.out.println("3 - Voltar");
+
+                        int opcaoInvestimento = scanner.nextInt();
+                        scanner.nextLine();
+
+                        switch (opcaoInvestimento) {
+                            case 1:
+                                cpfCriptografado= Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                                cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+                                try {
+                                    String poupanca = stub.investirPoupanca(ipCliente, cpfCriptografado);
+                                    poupanca = AES.decifrar(poupanca, chave.CHAVE_AES);
+                                    poupanca = Vernam.decifrar(poupanca, chave.CHAVE_VERNAM);
+
+                                    System.out.println("Seu saldo atual é: " + String.format("%.2f", Double.parseDouble(poupanca.split("-")[0]))
+                                            + "\n O rendimento aplicado na poupança em 3 meses é de: " + String.format("%.2f", Double.parseDouble(poupanca.split("-")[1]))
+                                            + "\n O rendimento aplicado na poupança em 6 meses é de: " + String.format("%.2f", Double.parseDouble(poupanca.split("-")[2]))
+                                            + "\n O rendimento aplicado na poupança em 12 meses é de: " + String.format("%.2f", Double.parseDouble(poupanca.split("-")[3])));
+
+                                }catch (Exception e){
+                                    throw new RuntimeException(e);
+                                }
+                                 break;
+                            case 2:
+                                System.out.println("Digite o valor: ");
+                                valor = scanner.nextLine();
+
+                                cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                                cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+                                valorCriptografado = Vernam.cifrar(valor, chave.CHAVE_VERNAM);
+                                valorCriptografado = AES.cifrar(valorCriptografado, chave.CHAVE_AES);
+                                try {
+                                    String renda = stub.investirRendaFixa(ipCliente, cpfCriptografado, valorCriptografado);
+                                    renda = AES.decifrar(renda, chave.CHAVE_AES);
+                                    renda = Vernam.decifrar(renda, chave.CHAVE_VERNAM);
+                                    System.out.println("Seu saldo atual é: " + String.format("%.2f", Double.parseDouble(renda.split("-")[0]))
+                                            + "\n O rendimento aplicado na renda fixa de "+ valor + " em 3 meses é de: " + String.format("%.2f", Double.parseDouble(renda.split("-")[1]))
+                                            + "\n O rendimento aplicado na renda fixa de "+ valor + " em 6 meses é de: " + String.format("%.2f", Double.parseDouble(renda.split("-")[2]))
+                                            + "\n O rendimento aplicado na renda fixa de "+ valor + " em 12 meses é de: " + String.format("%.2f", Double.parseDouble(renda.split("-")[3])));
+
+                                } catch (RemoteException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                break;
+                            case 3:
+                                break;
+                        }
+                        break;
+                case 6:
+                    break;
+            }
+        }else{
+            System.out.println("Bem vindo ao banco, escolha uma opção: ");
             System.out.println("1 - Cadastrar");
             System.out.println("2 - Logar");
-            System.out.println("3 - Sacar");
-            System.out.println("4 - Depositar");
-            System.out.println("5 - Transferir");
-            System.out.println("6 - Saldo");
-            System.out.println("7 - Investir na Poupança");
-            System.out.println("8 - Investir na Renda Fixa");
-            System.out.println("9 - Sair");
+            System.out.println("3 - Sair");
 
             int opcao = scanner.nextInt();
             scanner.nextLine();
 
-            switch (opcao) {
+            switch (opcao){
                 case 1:
                     System.out.println("Digite o nome: ");
-                    String nome = scanner.nextLine();
+                    nome = scanner.nextLine();
                     System.out.println("Digite o CPF: ");
-                    String cpf = scanner.nextLine();
+                    cpf = scanner.nextLine();
                     System.out.println("Digite a senha: ");
                     String senha = scanner.nextLine();
                     System.out.println("Digite o email: ");
@@ -62,8 +212,8 @@ public class Main {
                     String saldo = "0.0";
 
                     String dados = nome + "-" + cpf + "-" + senha + "-" + email + "-" + saldo;
-                    String dadosCriptografados = Vernam.cifrar(dados, chave.CHAVE_AES);
-                    dadosCriptografados = AES.cifrar(dadosCriptografados, chave.CHAVE_VERNAM);
+                    String dadosCriptografados = Vernam.cifrar(dados, chave.CHAVE_VERNAM);
+                    dadosCriptografados = AES.cifrar(dadosCriptografados, chave.CHAVE_AES);
 
                     try {
                         stub.cadastro(ipCliente, dadosCriptografados);
@@ -80,81 +230,26 @@ public class Main {
                     System.out.println("Digite a senha: ");
                     senha = scanner.nextLine();
 
-                    String cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_AES);
-                    cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_VERNAM);
-                    String senhaCriptografada = Vernam.cifrar(senha, chave.CHAVE_AES);
-                    senhaCriptografada = AES.cifrar(senhaCriptografada, chave.CHAVE_VERNAM);
+                    cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_VERNAM);
+                    cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_AES);
+                    String senhaCriptografada = Vernam.cifrar(senha, chave.CHAVE_VERNAM);
+                    senhaCriptografada = AES.cifrar(senhaCriptografada, chave.CHAVE_AES);
 
                     try {
-                        stub.logar(ipCliente, cpfCriptografado, senhaCriptografada);
+                        String conta = stub.logar(ipCliente, cpfCriptografado, senhaCriptografada);
+                        conta = AES.decifrar(conta, chave.CHAVE_AES);
+                        conta = Vernam.decifrar(conta, chave.CHAVE_VERNAM);
+
+                        autenticado = true;
+                        nome = conta.split("-")[0];
+                        cpf = conta.split("-")[1];
+
                     } catch (RemoteException e) {
                         throw new RuntimeException(e);
                     }
-                    break;
-                case 3:
-                    System.out.println("Digite o CPF: ");
-                    cpf = scanner.nextLine();
-                    System.out.println("Digite o valor: ");
-                    String valor = scanner.nextLine();
 
-                    cpfCriptografado = Vernam.cifrar(cpf, chave.CHAVE_AES);
-                    cpfCriptografado = AES.cifrar(cpfCriptografado, chave.CHAVE_VERNAM);
-                    String valorCriptografado = Vernam.cifrar(valor, chave.CHAVE_AES);
-                    valorCriptografado = AES.cifrar(valorCriptografado, chave.CHAVE_VERNAM);
-
-                    try {
-                        stub.sacar(ipCliente, cpfCriptografado, valorCriptografado);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
                     break;
-                case 4:
-                    System.out.println("Digite o CPF: ");
-                    cpf = scanner.nextLine();
-                    System.out.println("Digite o valor: ");
-                    valor = scanner.nextLine();
-                    try {
-                        stub.depositar(ipCliente, cpf, valor);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                case 5:
-                    System.out.println("Digite a conta de origem: ");
-                    String contaOrigem = scanner.nextLine();
-                    System.out.println("Digite a conta de destino: ");
-                    String contaDestino = scanner.nextLine();
-                    System.out.println("Digite o valor: ");
-                    valor = scanner.nextLine();
-                    try {
-                        stub.transferir(ipCliente, contaOrigem, contaDestino, valor);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                case 6:
-                    System.out.println("Digite o CPF: ");
-                    cpf = scanner.nextLine();
-                    try {
-                        stub.getSaldo(ipCliente, cpf);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                case 7:
-                    System.out.println("Digite o CPF: ");
-                    cpf = scanner.nextLine();
-                    System.out.println("Digite o valor: ");
-                    valor = scanner.nextLine();
-                    try {
-                        stub.investirPoupanca(" ", cpf, valor);
-                    } catch (RemoteException e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                case 8:
-                    System.out.println("Digite o CPF: ");
-                    cpf = scanner.nextLine();
+            }
             }
         }
     }
